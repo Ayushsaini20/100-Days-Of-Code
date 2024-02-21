73d344acffe6675bc88966fb3df444f67ac56b9a
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2024-02-21 11:10:10 +0530

    Solution.cpp

---
+++ DAY 39- Bitwise AND of Numbers Range/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count = 0; 
+        
+        while (left < right) {
+            left >>= 1; 
+            right >>= 1; 
+            count++; 
+        }
+        
+        return left << count; 
+    }
+};
