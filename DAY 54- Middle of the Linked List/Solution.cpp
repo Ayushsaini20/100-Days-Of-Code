3f7671a907e7b88a7971f031faa5f21257d689de
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2024-03-07 10:47:05 +0530

    Solution.cpp

---
+++ DAY 54- Middle of the Linked List/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slowPointer = head;
+        ListNode* fastPointer = head;
+
+        while (fastPointer != nullptr && fastPointer->next != nullptr) {
+            slowPointer = slowPointer->next;
+            fastPointer = fastPointer->next->next;
+        }
+
+        return slowPointer;
+    }
+};
