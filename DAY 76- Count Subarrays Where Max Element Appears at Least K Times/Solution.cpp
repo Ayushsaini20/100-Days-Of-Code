7020d0b21559819425f5ed77a6634e28b93305c5
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2024-03-31 11:36:03 +0530

    Solution.cpp

---
+++ DAY 76- Count Subarrays Where Max Element Appears at Least K Times/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        long long int max_num = *max_element(nums.begin(),nums.end()),count = 0;
+        long long int left = 0,right = 0,ans = 0;
+        while(right<nums.size()){
+            if(nums[right] == max_num)count++;
+            
+            while(count>=k){
+                if(nums[left]==max_num)count--;
+                left++;
+                ans += nums.size()-right;
+            }    
+            right++;
+        }
+
+        return ans;
+        
+    }
+};
