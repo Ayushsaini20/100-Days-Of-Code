5de8471bc4df910cb4388913ac480cdeab3d4f27
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2024-04-05 10:13:42 +0530

    Solution.cpp

---
+++ DAY 83- Make The String Great/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    std::string makeGood(std::string s) {
+        std::stack<char> stack;
+        
+        for (char c : s) {
+            if (!stack.empty() && std::abs(c - stack.top()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        std::string result;
+        while (!stack.empty()) {
+            result = stack.top() + result;
+            stack.pop();
+        }
+        
+        return result;
+    }
+};
