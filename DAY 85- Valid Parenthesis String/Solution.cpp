b84f75adf7ad45f35479d488073f2454834d4ab9
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2024-04-07 15:35:22 +0530

    Solution.cpp

---
+++ DAY 85- Valid Parenthesis String/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        
+        return leftMin == 0;
+    }
+};
